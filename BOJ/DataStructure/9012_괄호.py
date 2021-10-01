4cc18b1f3f0248a6b3a70aa5d85ee041961aa03d
MinguKang98 <alsrn7590@naver.com>
2021-10-01 23:14:50 +0900

    20211001 #BOJ_9012 solve

---
+++ BOJ/DataStructure/9012_괄호.py
@@ -0,0 +1,42 @@
+# 9012_괄호
+import sys
+
+
+# list를 사용
+def find_vps(pss: str):
+    stack = []
+    for ps in pss:
+        if ps == "(":
+            stack.append("(")
+        else:
+            # top of stack 이 ")" or empty 면 ")" 추가
+            if len(stack) == 0 or stack[-1] == ")":
+                stack.append(")")
+            # top of stack 이 "(" 면 remove top
+            else:
+                stack.pop(-1)
+    if len(stack) == 0:
+        print("YES")
+    else:
+        print("NO")
+
+
+# index를 사용
+def find_vps2(pss: str):
+    stack = 0
+    for ps in pss:
+        if ps == "(":
+            stack += 1
+        else:
+            stack -= 1
+            if stack < 0:
+                break
+    if stack == 0:
+        print("YES")
+    else:
+        print("NO")
+
+
+N = int(input())
+for i in range(N):
+    find_vps2(sys.stdin.readline().strip())
