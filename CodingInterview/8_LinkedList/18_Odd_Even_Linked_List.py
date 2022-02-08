270c38417ba6c0138e408cc5516429e5e54c81ba
MinguKang98 <alsrn7590@naver.com>
2022-02-09 01:38:00 +0900

    20220209 #CodingInterview 08_18 solve

---
+++ CodingInterview/8_LinkedList/18_Odd_Even_Linked_List.py
@@ -0,0 +1,46 @@
+# 18_Odd_Even_Linked_List
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    # Solution0 - my code
+    def oddEvenList0(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        ord_head = head
+        temp_root = temp_head = ListNode(None)
+
+        while ord_head and ord_head.next:
+            even_head = ord_head.next
+            ord_head.next = even_head.next
+            even_head.next = None
+            temp_head.next = even_head
+
+            ord_head = ord_head.next
+            temp_head = temp_head.next
+
+        ord_head.next = temp_root.next
+        return head
+
+    """
+    예시는 통과하나 코트 에러 나옴
+    """
+
+    # Solutino1 - change iterative
+    def oddEvenList1(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return head
+
+        odd = head
+        even_head = even = head.next
+
+        while even and even.next:
+            odd.next, even.next = odd.next.next, even.next.next
+            odd, even = odd.next, even.next
+
+        odd.next = even_head
+        return head
