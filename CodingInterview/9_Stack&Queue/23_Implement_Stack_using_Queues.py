4216281a168c81f75c31dd72c74930610336a0c3
MinguKang98 <alsrn7590@naver.com>
2022-03-11 01:27:16 +0900

    20220311 #CodingInterview 09_23 solve

---
+++ CodingInterview/9_Stack&Queue/23_Implement_Stack_using_Queues.py
@@ -0,0 +1,29 @@
+# 23_Implement_Stack_using_Queues
+from collections import deque
+
+
+class MyStack:
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+
+myStack = MyStack()
+myStack.push(1)
+myStack.push(2)
+myStack.top()
+myStack.pop()
+myStack.empty()
