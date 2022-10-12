82041398bcb6f141f1bf4f467b42a82352540985
mingu <alsrn7590@naver.com>
2022-10-12 11:34:20 +0900

    20221012 #CodingInterview 19_70 solve

---
+++ CodingInterview/19_BitManipulation/70_Single_Number.py
@@ -0,0 +1,36 @@
+# 70_Single_Number
+# https://leetcode.com/problems/single-number/
+import collections
+from typing import List
+
+
+class Solution:
+    # Solution0 - using counter
+    def singleNumber0(self, nums: List[int]) -> int:
+        counter = collections.Counter(nums)
+        result = min(counter, key=counter.get)
+        return result
+
+    """
+    counter 사용하여 풀이
+    min은 기본적으로 key 값을 기준으로 연산하므로 min 의 key 를 counter.get 으로 주어
+    counter 의 value가 가장 작은 key 값을 찾도록 함
+    result = value 값이 가장 작은 key
+    """
+
+    # Solution1 - using xor
+    def singleNumber0(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
+
+    """
+    xor 은 입력값이 다르면 true 같으면 false 이다
+    두 번 등장하면 0으로 초기화 되고 한번만 등장하면 그 값을 보존한다.
+    따라서 nums 에 대해 모두 xor 연산을 하면 single number 만 남게 된다.
+    """
+
+
+nums = [4, 1, 2, 1, 2]
+print(Solution().singleNumber0(nums))
